82da4427b4981e2736487aac5a612ac70bd9a630
Hari <92938055+NateNear@users.noreply.github.com>
2023-10-28 00:50:06 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -97,7 +97,7 @@ const Header = () => {
                     </div>
                 </div>
                 <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
-                    <img src="https://xsgames.co/randomusers/assets/avatars/female/67.jpg" />
+                    <img src="https://xsgames.co/randomusers/assets/avatars/female/67.jpg" alt="image" />
                 </div>
             </div>
         </div>
